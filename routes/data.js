cd09f8a7b2720b0ca5ef595defe3e3a9bd6a7c2c
Emmy Chen <emmyc3@uci.edu>
2024-01-10 13:15:17 -0800

    Committing fixed table

--- routes/data.js
+++ routes/data.js
@@ -9,23 +9,41 @@ require('dotenv').config();
 dataRouter.use(express.json());
 
 dataRouter.get('/', async (req, res) => {
-  // Reutn all data from volunteers table
+  // Return all data from event_data table
   try {
-    const volunteers = await db.query('SELECT * FROM volunteers');
-    res.status(200).send(volunteers.rows);
+    const events = await db.query('SELECT * FROM event_data');
+    res.status(200).send(events.rows);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
 dataRouter.post('/', async (req, res) => {
-  // Add new volunteer to volunteers table, requires volunteer info in body
+  // Add new event to event_data table, requires event info in body
   try {
-    const { id, email, first_name, last_name } = req.body;
+    const {
+      id,
+      volunteer_id,
+      number_in_party,
+      pounds,
+      ounces,
+      unusual_items,
+      event_id,
+      is_checked_in,
+    } = req.body;
     const postQuery =
-      'INSERT INTO volunteers (id, email, first_name, last_name) VALUES ($1, $2, $3, $4);';
-    const volunteerData = [id, email, first_name, last_name];
-    const insertedStatus = await db.query(postQuery, volunteerData);
+      'INSERT INTO event_data (id, volunteer_id, number_in_party, pounds, ounces, unusual_items, event_id, is_checked_in) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);';
+    const eventData = [
+      id,
+      volunteer_id,
+      number_in_party,
+      pounds,
+      ounces,
+      unusual_items,
+      event_id,
+      is_checked_in,
+    ];
+    const insertedStatus = await db.query(postQuery, eventData);
     res.status(200).send(insertedStatus);
   } catch (err) {
     res.status(500).send(err.message);
@@ -33,50 +51,37 @@ dataRouter.post('/', async (req, res) => {
 });
 
 dataRouter.put('/:id', async (req, res) => {
-  // Update volunteer data by ID
+  // Update event data by ID
   try {
-    const { email, first_name, last_name } = req.body;
-    // Data to update passed through body, data not to be updated left blank
+    const {
+      volunteer_id,
+      number_in_party,
+      pounds,
+      ounces,
+      unusual_items,
+      event_id,
+      is_checked_in,
+    } = req.body;
+    // Data to update passed through body
     const { id } = req.params;
     // ID passed as parameter
 
-    if (id == null) {
+    if (id === undefined) {
       // ID must be passed
       res.status(400).send('Invalid ID');
     } else {
-      let putQuery = 'UPDATE volunteers SET ';
-      let paramater_num = 1;
-      const parameterValues = [];
-      if (id === undefined) {
-        res.status(500);
-      }
-      if (email !== undefined) {
-        // Update email if it is defined in body
-        putQuery += `email = $${paramater_num} `;
-        parameterValues.push(email);
-        paramater_num += 1;
-      }
-      if (first_name !== undefined) {
-        // Update first_name if it is defined in the body
-        if (paramater_num > 1) {
-          putQuery += ', ';
-        }
-        putQuery += `first_name = $${paramater_num} `;
-        parameterValues.push(first_name);
-        paramater_num += 1;
-      }
-      if (last_name !== undefined) {
-        // Update last_name if it is defined in body
-        if (paramater_num > 1) {
-          putQuery += ', ';
-        }
-        putQuery += `last_name = $${paramater_num} `;
-        parameterValues.push(last_name);
-        paramater_num += 1;
-      }
-
-      putQuery += `WHERE id = $${paramater_num}`;
-      parameterValues.push(id);
+      const putQuery =
+        'UPDATE event_data SET volunteer_id = $1, number_in_party = $2, pounds = $3, ounces = $4, unusual_items = $5, event_id = $6, is_checked_in = $7 WHERE id = $8';
+      const parameterValues = [
+        volunteer_id,
+        number_in_party,
+        pounds,
+        ounces,
+        unusual_items,
+        event_id,
+        is_checked_in,
+        id,
+      ];
       const inserted = await db.query(putQuery, parameterValues);
       res.status(200).send(inserted);
     }
@@ -86,12 +91,11 @@ dataRouter.put('/:id', async (req, res) => {
 });
 
 dataRouter.delete('/:id', async (req, res) => {
-  // Delete volunteer by ID
+  // Delete event by ID
   try {
     const { id } = req.params;
-    const delQuery = 'DELETE FROM volunteers WHERE id = $1';
+    const delQuery = 'DELETE FROM event_data WHERE id = $1';
     const delId = [id];
-
     const deleteStatus = await db.query(delQuery, delId);
     res.status(200).send(deleteStatus);
   } catch (err) {
