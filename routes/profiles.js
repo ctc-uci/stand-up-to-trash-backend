7541dc3edd1eb79eff4047278fd85365957ecb68
Ostend Suryajaya <osuryaja@uci.edu>
2024-01-10 11:39:59 -0800

    Fixed prettier formatting

--- routes/profiles.js
+++ routes/profiles.js
@@ -6,6 +6,7 @@ const profilesRouter = express.Router();
 const pool = require('../server/db');
 
 profilesRouter.use(express.json());
+
 profilesRouter.post('/', async (req, res) => {
   try {
     const { id, email, first_name, last_name } = req.body;
@@ -13,18 +14,18 @@ profilesRouter.post('/', async (req, res) => {
       'INSERT INTO volunteers (id, email, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *',
       [id, email, first_name, last_name],
     );
-    res.json(newProfile.rows[0]);
+    res.status(201).json(newProfile.rows[0]);
   } catch (error) {
-    res.json(error);
+    res.status(400).json(error);
   }
 });
 
 profilesRouter.get('/', async (req, res) => {
   try {
     const allProfiles = await pool.query('SELECT * FROM volunteers');
-    res.json(allProfiles.rows);
+    res.status(200).json(allProfiles.rows);
   } catch (error) {
-    res.json(error);
+    res.status(400).json(error);
   }
 });
 
@@ -32,9 +33,9 @@ profilesRouter.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const profile = await pool.query('SELECT * FROM volunteers WHERE id = $1', [id]);
-    res.json(profile.rows[0]);
+    res.status(200).json(profile.rows[0]);
   } catch (error) {
-    res.json(error);
+    res.status(400).json(error);
   }
 });
 
@@ -46,7 +47,7 @@ profilesRouter.put('/:id', async (req, res) => {
       'UPDATE volunteers SET email = $1, first_name = $2, last_name = $3 WHERE id = $4 RETURNING *',
       [email, first_name, last_name, id],
     );
-    res.json(updateProfile.rows[0]);
+    res.status(200).json(updateProfile.rows[0]);
   } catch (error) {
     res.json(error);
   }
