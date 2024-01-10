2e4d12f04ba25ccfb09424d3433e5adc171651eb
Emmy Chen <emmyc3@uci.edu>
2024-01-09 17:17:56 -0800

    Committing start to put

--- routes/data.js
+++ routes/data.js
@@ -30,4 +30,50 @@ dataRouter.post('/', async (req, res) => {
   }
 });
 
+dataRouter.put('/:id', async (req, res) => {
+  try {
+    const { id, email, first_name, last_name } = req.body;
+    if (id == null) {
+      res.status(400).send('Invalid ID');
+    } else {
+      /* const putQuery =
+        'UPDATE volunteers SET email = $1, first_name = $2, last_name = $3 WHERE id = $4;'; */
+      let putQuery = 'UPDATE volunteers SET ';
+      const update_volunteer = [email, first_name, last_name, id];
+      let num = 1;
+      const values = [];
+      if (id == null) {
+        res.status(500);
+      }
+      if (email !== null) {
+        putQuery += `email = $${num} `;
+        values.push(email);
+        num += 1;
+      }
+      if (first_name !== null) {
+        if (num > 1) {
+          putQuery += ', ';
+        }
+        putQuery += `first_name = $${num} `;
+        values.push(first_name);
+        num += 1;
+      }
+      if (last_name !== null) {
+        if (num > 1) {
+          putQuery += ', ';
+        }
+        putQuery += `last_name = $${num} `;
+        values.push(last_name);
+        num += 1;
+      }
+
+      putQuery += `WHERE id = $${num}; `;
+      const inserted = await db.query(putQuery, update_volunteer);
+      res.status(200).send(inserted);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 module.exports = dataRouter;
