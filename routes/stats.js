7c05e0af9993b636a4e3a9e4791276bd4241b7f4
Emmy Chen <emmyc3@uci.edu>
2024-01-20 13:20:34 -0800

    Committing checkin put

--- routes/stats.js
+++ routes/stats.js
@@ -2,11 +2,21 @@
 const express = require('express');
 
 const statsRouter = express.Router();
-// const pool = require('../server/db');
+const pool = require('../server/db');
+
 statsRouter.use(express.json());
 
-statsRouter.get('/', async (req, res) => {
-  res.send('hehe');
+statsRouter.put('/data/checkin/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const allEvents = await pool.query(
+      'UPDATE event_data SET is_checked_in= NOT is_checked_in WHERE id=$1',
+      [id],
+    );
+    res.status(200).json(allEvents.rows);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = statsRouter;
