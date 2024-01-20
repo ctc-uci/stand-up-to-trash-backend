8bf6f465ccdc15d2b6d01ba1adb48c143eaf50c7
Katy Huang <katyhuang360166@gmail.com>
2024-01-19 21:45:24 -0800

    feat: Implemented three new endpoints to calculate the total amount of collected trash (total,by event, by volunteer)

--- routes/stats.js
+++ routes/stats.js
@@ -2,11 +2,49 @@
 const express = require('express');
 
 const statsRouter = express.Router();
-// const pool = require('../server/db');
+
+const pool = require('../server/db');
+
 statsRouter.use(express.json());
 
 statsRouter.get('/', async (req, res) => {
-  res.send('hehe');
+  try {
+    const response = await pool.query('SELECT SUM(pounds) FROM event_data');
+    const totalTrash =
+      response.rows[0].sum != null ? parseFloat(response.rows[0].sum).toFixed(2) : '0.00';
+    res.json(totalTrash);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+statsRouter.get('/event/:eventId', async (req, res) => {
+  try {
+    const { eventId } = req.params;
+    const response = await pool.query('SELECT SUM(pounds) FROM event_data WHERE event_id = $1', [
+      eventId,
+    ]);
+    const totalTrash =
+      response.rows[0].sum != null ? parseFloat(response.rows[0].sum).toFixed(2) : '0.00';
+    res.json(totalTrash);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+statsRouter.get('/volunteer/:volunteerId', async (req, res) => {
+  try {
+    const { volunteerId } = req.params;
+    const response = await pool.query(
+      'SELECT SUM(pounds) FROM event_data WHERE volunteer_id = $1',
+      [volunteerId],
+    );
+    const totalTrash =
+      response.rows[0].sum != null ? parseFloat(response.rows[0].sum).toFixed(2) : '0.00';
+    res.json(totalTrash);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 module.exports = statsRouter;
