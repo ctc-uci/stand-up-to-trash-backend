8c2d267ce1df5f2808dd41ccebfa9202d74d462d
Brendan Lieu <brendanlieu05@gmail.com>
2024-02-02 21:58:05 -0800

    Switched the intervals in the SQL ANDs

--- routes/stats.js
+++ routes/stats.js
@@ -50,11 +50,11 @@ statsRouter.get('/volunteer/:volunteerId', async (req, res) => {
 statsRouter.get('/week', async (req, res) => {
   try {
     const response = await pool.query(
-      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE AND CURRENT_DATE - INTERVAL '7 days'`,
+      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '7 days' AND CURRENT_DATE`,
     );
     const pounds = parseFloat(response.rows[0].pound_sum);
     const ounces = parseFloat(response.rows[0].ounces_sum);
-    const total = pounds + ounces / 16;
+    const total = (pounds + ounces / 16).toFixed(2);
     res.json(total);
   } catch (err) {
     res.status(400).json(err);
@@ -64,19 +64,19 @@ statsRouter.get('/week', async (req, res) => {
 statsRouter.get('/year', async (req, res) => {
   try {
     const response = await pool.query(
-      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE AND CURRENT_DATE - INTERVAL '1 year'`,
+      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '1 year' AND CURRENT_DATE`,
     );
     const pounds = parseFloat(response.rows[0].pound_sum);
     const ounces = parseFloat(response.rows[0].ounces_sum);
     const total = pounds + ounces / 16;
 
     const responseTwo = await pool.query(
-      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '1 year' AND CURRENT_DATE - INTERVAL '2 years'`,
+      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '2 years' AND CURRENT_DATE - INTERVAL '1 year'`,
     );
     const poundsTwo = parseFloat(responseTwo.rows[0].pound_sum);
     const ouncesTwo = parseFloat(responseTwo.rows[0].ounces_sum);
     const totalTwo = poundsTwo + ouncesTwo / 16;
-    const rate = (totalTwo - total) / total;
+    const rate = (((totalTwo - total) / total) * 100).toFixed(2);
     res.json(rate);
   } catch (err) {
     res.status(400).json(err);
@@ -86,20 +86,20 @@ statsRouter.get('/year', async (req, res) => {
 statsRouter.get('/month', async (req, res) => {
   try {
     const response = await pool.query(
-      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE AND CURRENT_DATE - INTERVAL '1 month'`,
+      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '1 month' AND CURRENT_DATE`,
     );
     const pounds = parseFloat(response.rows[0].pound_sum);
     const ounces = parseFloat(response.rows[0].ounces_sum);
     const total = pounds + ounces / 16;
 
     const responseTwo = await pool.query(
-      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '1 month' AND CURRENT_DATE - INTERVAL '3 months'`,
+      `SELECT SUM(pounds) AS pound_sum, SUM(ounces) AS ounces_sum FROM event_data INNER JOIN events on event_data.event_id=events.id WHERE events.date BETWEEN CURRENT_DATE - INTERVAL '3 months' AND CURRENT_DATE - INTERVAL '1 month' `,
     );
     const poundsTwo = parseFloat(responseTwo.rows[0].pound_sum);
     const ouncesTwo = parseFloat(responseTwo.rows[0].ounces_sum);
     const totalTwo = poundsTwo + ouncesTwo / 16;
-    const rate = (totalTwo - total) / total;
-    res.json(rate);
+    const rate = ((totalTwo - total) / total).toFixed(2);
+    res.json(totalTwo);
   } catch (err) {
     res.status(400).json(err);
   }
