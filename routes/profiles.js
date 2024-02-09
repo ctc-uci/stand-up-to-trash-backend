9fb56cd849c3d5a30a91827342fd4ca4ce619c90
jessieh9 <104229166+jessieh9@users.noreply.github.com>
2024-02-08 21:31:29 -0800

    fixing profiles for user table

--- routes/profiles.js
+++ routes/profiles.js
@@ -9,10 +9,12 @@ profilesRouter.use(express.json());
 
 profilesRouter.post('/', async (req, res) => {
   try {
-    const { id, email, first_name, last_name } = req.body;
+    const { first_name, last_name, role, email, firebase_uid } = req.body;
+    // change this to new db
+    // first_name, last_name, role, email, firebase_uid, image_url
     const newProfile = await pool.query(
-      'INSERT INTO volunteers (id, email, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING *',
-      [id, email, first_name, last_name],
+      'INSERT INTO users (first_name, last_name, role, email, firebase_uid) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [first_name, last_name, role, email, firebase_uid],
     );
     res.status(201).json(newProfile.rows[0]);
   } catch (error) {
@@ -22,7 +24,7 @@ profilesRouter.post('/', async (req, res) => {
 
 profilesRouter.get('/', async (req, res) => {
   try {
-    const allProfiles = await pool.query('SELECT * FROM volunteers');
+    const allProfiles = await pool.query('SELECT * FROM users');
     res.status(200).json(allProfiles.rows);
   } catch (error) {
     res.status(400).json(error);
@@ -32,7 +34,7 @@ profilesRouter.get('/', async (req, res) => {
 profilesRouter.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const profile = await pool.query('SELECT * FROM volunteers WHERE id = $1', [id]);
+    const profile = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
     res.status(200).json(profile.rows[0]);
   } catch (error) {
     res.status(400).json(error);
@@ -42,9 +44,9 @@ profilesRouter.get('/:id', async (req, res) => {
 profilesRouter.put('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const { email, first_name, last_name } = req.body;
+    const { first_name, last_name, role, email, firebase_uid, image_url } = req.body;
     const updateProfile = await pool.query(
-      'UPDATE volunteers SET email = $1, first_name = $2, last_name = $3 WHERE id = $4 RETURNING *',
+      'UPDATE users SET first_name = $1, last_name = $2, role = $3, email = $4 WHERE id = $5 RETURNING *',
       [email, first_name, last_name, id],
     );
     res.status(200).json(updateProfile.rows[0]);
@@ -57,10 +59,9 @@ profilesRouter.put('/:id', async (req, res) => {
 profilesRouter.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteProfileResult = await pool.query(
-      'DELETE FROM volunteers WHERE id = $1 RETURNING *',
-      [id],
-    );
+    const deleteProfileResult = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [
+      id,
+    ]);
 
     if (deleteProfileResult.rowCount === 0) {
       return res.status(404).json({ message: 'Profile not found' });
